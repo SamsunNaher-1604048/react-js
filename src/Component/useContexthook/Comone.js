9946e94ce52de74f3fc31136336c5570305b7122
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-06-29 21:58:26 +0600

    minor change

--- src/Component/useContexthook/Comone.js
+++ src/Component/useContexthook/Comone.js
@@ -5,9 +5,10 @@ import { Usercontext } from './Usecontext'
 
 export const Comone = () => {
     const[user,setuser]=useState({id:1, name:"nishi"})
+    const[data,sedata]=useState("hello i am nishi")
   return (
    
-        <Usercontext.Provider value={{user}}>
+        <Usercontext.Provider value={{user,data}}>
             <Comtwo/>
         </Usercontext.Provider>
     
--- src/Component/useContexthook/Comthree.js
+++ src/Component/useContexthook/Comthree.js
@@ -2,12 +2,14 @@ import React,{useContext} from 'react'
 import { Usercontext } from './Usecontext';
 
 const Comthree = () => {
-    const user=useContext(Usercontext)
+    const {user,data}=useContext(Usercontext)
     console.log(user)
+    console.log(data)
   return (
     <div>
         <h1>{user.id}</h1>
         <h1>{user.name}</h1>
+        <h1>{data}</h1>
     </div>
   )
 }
