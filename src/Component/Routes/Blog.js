21a366cab4281faf7e822ffddfa4575e95e99c32
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-06-30 22:14:13 +0600

    route

---
+++ src/Component/Routes/Blog.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>
+        <h1>blog</h1>
+        <p>A Little Life follows a chronological narrative with flashbacks frequently interspersed throughout.
+             The novel's narrative perspectives shift throughout the story's progression. During the beginning of the novel, 
+             a third-person omniscient perspective privileging the thoughts of Jude, Willem, JB and Malcolm is employed. As 
+             the story gradually shifts its focus towards Jude, its perspective progressively molds entirely around each character's 
+             interactions with Jude and the experiences of Jude himself. This literary perspective is punctuated by first-
+             person narratives told by an older Harold, nine years in the future.</p>
+      
+    </div>
+  )
+}
+
+export default Blog
