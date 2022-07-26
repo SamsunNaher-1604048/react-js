0ba12983ec1171acd8d4f012f9e2a78a4e604fed
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2022-07-26 08:32:13 +0600

    memo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import React, { useState } from "react";
+import Message from "./Component/Meno()/Message";
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 
-import Blog from "./Component/Routes/Blog";
-import Conact from "./Component/Routes/Conact";
-import Home from "./Component/Routes/Home";
+// import Blog from "./Component/Routes/Blog";
+// import Conact from "./Component/Routes/Conact";
+// import Home from "./Component/Routes/Home";
 //import { Comone } from "./Component/useContexthook/Comone";
 //import Userform from "./Component/useRef/Userform";
 
 
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/conact" element={<Conact/>}/>
-        <Route path='/blog' element={<Blog/>}/>
+  const[count,Setcount]=useState(0)
 
-      </Routes>
-    </BrowserRouter>
+  const increment=()=>{
+    Setcount(count+1)
+  }
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home/>}/>
+    //     <Route path="/conact" element={<Conact/>}/>
+    //     <Route path='/blog' element={<Blog/>}/>
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <h1>count:{count}</h1>
+      <button onClick={increment}>increment</button>
+      <Message/>
+    </div>
   );
 }
